26681d3db44cf7bb1d94433382edda4ffd604d8c
God-freyMoses <godfreymoses203@gmail.com>
2025-06-10 16:31:21 +0200

    Phushed the accounts, will take it from home

--- banking-system/src/main/java/bank/accounts/Account.java
+++ banking-system/src/main/java/bank/accounts/Account.java
@@ -11,5 +11,22 @@ public abstract class Account {
     protected double balance;
     private final List<Transaction> transactionHistory;
 
+    public Account(String holderName, double initialBalance) {
+        this.accountNumber = ++accountCounter;
+        this.holderName = holderName;
+        this.balance = initialBalance;
+        this.transactionHistory = new ArrayList<>();
+    }
+    public int getAccountNumber() { return accountNumber; }
+    public String getHolderName() { return holderName; }
+    public void setHolderName(String name) { this.holderName = name; }
+    public double getBalance() { return balance; }
+    public List<Transaction> getTransactionHistory() { return transactionHistory; }
+
+    public void addTransaction(Transaction t) {
+        transactionHistory.add(t);
+    }
+
+
 
 }
--- banking-system/src/main/java/bank/accounts/CheckingAccount.java
+++ banking-system/src/main/java/bank/accounts/CheckingAccount.java
@@ -9,5 +9,18 @@ public class CheckingAccount extends Account {
     public CheckingAccount (String holderName, double initialBalance){
         super(holderName, initialBalance);
     }
-    
+    @Override
+    public void deposit(double amount) {
+        if (amount <= 0) throw new IllegalArgumentException("Deposit must be positive");
+        balance += amount;
+
+    }
+    @Override
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount <= 0) throw new IllegalArgumentException("Withdrawal must be positive");
+        if (amount > balance + OVERDRAFT_LIMIT)
+            throw new InsufficientFundsException("Overdraft limit exceeded");
+        balance -= amount;
+
+    }
 }
